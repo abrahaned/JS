85d6e34c47de82f8a9735adff989400d786d2d03
abrahaned <personprivate701@gmail.com>
2025-01-01 17:23:37 +0630

    learn var scope

---
+++ Variables/var.js
@@ -0,0 +1,17 @@
+var x = 22;
+console.log(window.x);  //22
+
+if(x == 22){
+    var x = 23;
+    console.log(x);//23
+}
+
+console.log(x);//23
+
+var y = "y";
+console.log(y);//y
+if(y == "y"){
+    y = "z"
+}
+
+console.log(y);//z
